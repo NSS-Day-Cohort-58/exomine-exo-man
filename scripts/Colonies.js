c9f4d96ef0dfc539382de280a4ab739336e40dc1
Lorin Jones <lorin.jones@gmail.com>
2022-08-16 12:48:07 -0600

    updated colonies and database mineral arrays

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -1,2 +1,19 @@
-import { getColonies, setColony } from "./database.js"
+import { getColonies } from "./database.js"
+
+const colonies = getColonies()
+
+export const Colonies = (governor) => {
+    let html = ""
+
+    const header = colonies.map(
+        (colony) => {
+            if (governor.colonyId === colony.id) {
+                return `<h2 class="colony">${colony.name} Minerals</h2>`
+            }
+        }
+    )
+    html += header
+    return html
+    
+}
 
